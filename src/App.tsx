8d80a52773d5b95fcad1f97ea4760bfa51fefb0d
handokei <ehgks994@gmail.com>
2026-02-13 15:13:58 +0900

    chore: 회원가입 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<div className="p-4">홈페이지 (로그인 후)</div>} />
       </Routes>
     </BrowserRouter>
