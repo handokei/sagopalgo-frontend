8a6620ab82659a0ee42644b019f90b011113c467
handokei <ehgks994@gmail.com>
2026-02-13 16:35:49 +0900

    chore: 주문 관련 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import RegisterPage from './pages/RegisterPage';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
+import OrderPage from './pages/OrderPage';
+import OrderListPage from './pages/OrderListPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<ProductListPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/orders" element={<OrderListPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
